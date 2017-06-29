e1a8f28e3ab7e81d662dfa00bca671861f04137f
Luana Martins <lms7@cin.ufpe.br>
2017-06-29 08:21:28 -0300

    Fix code formatting

--- poll/urls.py
+++ poll/urls.py
@@ -2,7 +2,7 @@ from poll import views
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-                       url(r'^$', views.PollList.as_view(), name='poll_list'),
-                       url(r'^(?P<pk>[0-9]+)/$', views.PollDetail.as_view(), name='poll_detail'),
-                       url(r'^(?P<q>.+)/$', views.SearchPoll.as_view(), name='search-poll'),
-                       )
+   url(r'^$', views.PollList.as_view(), name='poll_list'),
+   url(r'^(?P<pk>[0-9]+)/$', views.PollDetail.as_view(), name='poll_detail'),
+   url(r'^(?P<q>.+)/$', views.SearchPoll.as_view(), name='search-poll'),
+)
